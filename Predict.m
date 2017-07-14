1d9b3be796b36bd23aa2db5b700999b92b54d71e
Jérémie Kalfon <jkobject@gmail.com>
2017-07-14 15:14:26 -0400

    adding comments

--- Predict.m
+++ Predict.m
@@ -15,66 +15,67 @@ function [Out,K,SNR]=Predict(Ka,va)
 
 warning('off','all')
 
-
+%initialisation of the values
 %Ka = imread('test.jpg');
-%va = 0.04;
+%va = 0.08;
 Ka = rgb2gray(Ka);
 K = imnoise(Ka,'gaussian',0,va);
-psize = 10;
+psize = 3;
 H = 43;
 W = 64;
-
 Patchs = zeros(2*psize+H,2*psize+W);
-
-Patchs(psize+1:end-psize,psize+1:end-psize) = K(:,:);
-
-Patchs(1:psize,:)= repmat(Patchs(psize+1,:),psize,1);
-Patchs(end-psize+1:end,:)= repmat(Patchs(end-psize,:),psize,1);
-Patchs(:,1:psize)= repmat(Patchs(:,psize+1),1,psize);
-Patchs(:,end-psize+1:end)= repmat(Patchs(:,end-psize),1,psize);
-
-Patchs = Patchs/255;
 size = 2*psize;
 R1 = zeros(size^2,size^2);
 V = va^2;
 pospix = round(((size+1)^2)/2);
 Out = zeros(H-1,W-1);
 Ss = zeros((H-1)*(W-1),1);
+
+%creating the patch to which we will create small patches 
+Patchs(psize+1:end-psize,psize+1:end-psize) = K(:,:);
+%we need more values on the side 
+Patchs(1:psize,:)= repmat(Patchs(psize+1,:),psize,1);
+Patchs(end-psize+1:end,:)= repmat(Patchs(end-psize,:),psize,1);
+Patchs(:,1:psize)= repmat(Patchs(:,psize+1),1,psize);
+Patchs(:,end-psize+1:end)= repmat(Patchs(:,end-psize),1,psize);
+%normalize
+Patchs = Patchs/255;
+
+
 for indx = 1:H-1
-    for indy = 1:W-1
+    for indy = 1:W-1 %for each pixel
         patch = Patchs(indx:indx+size,indy:indy+size);
-        M = mean2(patch)^2;
+        M = mean2(patch)^2; % we compute the important values of the patch 
         S = std2(patch);
-        Ss(indx*indy) = S;
+        Ss(indx*indy) = S; % to compute SNR
         S= S^2;
-        for xIter = 1:size+1
+        for xIter = 1:size+1 
             for yIter = 1:size+1
                 for xIt = 1:size+1
-                    for yIt = 1:size+1
+                    for yIt = 1:size+1 % correlate each pixel to each other pixel 
                         if(xIt*yIt~=xIter*yIter)
                              R1(xIt*yIt, xIter*yIter) = M + S*exp((-sqrt((xIt-xIter)^2 + (yIt-yIter)^2)/psize)^2);
-                        else
+                        else  % if its the self there is the importnace of the noise as well
                              R1(xIt*yIt,xIt*yIt) =  M+S+V;
                         end
                     end
                 end
             end
         end
+        % removes the values to create the system
         rhs = R1(pospix,:)';
-        R = R1([1:pospix-1, pospix+1:end],:);
-        
-        h1 = linsolve(R', rhs);
+        R = R1([1:pospix-1, pospix+1:end],:)';
+
+        h1 = linsolve(R, rhs); % the system resolving 
         h = [h1(1:pospix-1); 0; h1(pospix:end)];
         
-        h(h<0)=0;
+        h(h<0)=0; % no negative values in the synapse's weight
         H1 = reshape(h, size+1, size+1);
         
-        out = K(indx,indy) - (sum(sum(H1.*patch))*255);
-        Out(indx,indy) =  out;
+        Out(indx,indy) = K(indx,indy) - (sum(sum(H1.*patch))*255);
     end 
 end
-Ss = mean(Ss);
-SNR = Ss/va;
+SNR = mean(Ss)/va;
 
 
 
